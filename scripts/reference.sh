58a5e4c95a606e97e43147158202331ae9853e44
Genalice <39982515+Genalice@users.noreply.github.com>
2018-06-06 14:37:28 +0200

    Create reference.sh

---
+++ scripts/reference.sh
@@ -0,0 +1,37 @@
+#!/bin/bash
+# reference.sh
+# bas.tolhuis@genalice.com
+#   prepare GRF and GRI prior to mapping
+#------------------------------------------------------------------------------
+# reference assemblies
+ref37=references/GRCh37.p13.genome.fa
+alt37=references/GRCh37.p13.genome.alt
+ref38=references/GCA_000001405.15_GRCh38_full_plus_hs38d1_analysis_set.fa
+#!/bin/bash
+# reference.sh
+# bas.tolhuis@genalice.com
+#   prepare GRF and GRI prior to mapping
+#------------------------------------------------------------------------------
+# reference assemblies
+assembly=GRCh37.p13.genome
+fasta=references/$assembly.fa
+altfile=references/G$assembly.alt
+
+#------------------------------------------------------------------------------
+# GRF/GRI files
+# create grf
+gaReference
+        --input=$fasta \
+        --output=references/$assembly.grf \
+        --alt_file=$altfile \
+        --report_file=log/$assembly.gareference.log \
+        --error_file=log/$assembly.gareference.err
+                
+ # create gri
+ gaIndex \
+        --input=references/$assembly.grf \
+        --output=references/$assembly.gri \
+        --report_file=log/$assembly.gaindex.log \
+        --error_file=log/$assembly.gaindex.err
+                
+#------------------------------------------------------------------------------
